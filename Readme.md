f0604f2dcea34482bf19849607229837594ba3a2
me@isaachawkins.com <me@isaachawkins.com>
2019-10-30 10:51:36 +1100

    initial

---
+++ Readme.md
@@ -0,0 +1,13 @@
+#Affine Cypher Decoder
+
+An Affine Cypher is a Cypher that generates an encoded string using the formula
+```C = aP + b```
+P is the original String
+a is a simple integer
+b is a simple integer
+
+
+It can be decoded with the following formula
+```P = aC + b```
+
+This Python Script is based around the basic algorithm for a Affine Cypher, and uses a brute force method and dictionary tester to make sure that it is decoding a valid english string. The task was given from [r/dailyprogrammer](https://www.reddit.com/r/dailyprogrammer/comments/6k123x/20170629_challenge_321_intermediate_affine_cipher/) as task on reddit and uses the dictionary provided by them.
---
+++ affine-cypher.py
@@ -0,0 +1,49 @@
+import time
+
+test_str = ["NLWC WC M NECN", "YEQ LKCV BDK XCGK EZ BDK UEXLVM QPLQGWSKMB", "NH WRTEQ TFWRX TGY T YEZVXH GJNMGRXX STPGX NH XRGXR TX QWZJDW ZK WRNUZFB P WTY YEJGB ZE RNSQPRY XZNR YJUU ZSPTQR QZ QWR YETPGX ZGR NPGJQR STXQ TGY URQWR VTEYX WTY XJGB"]
+test_solution_str = ["this is a test", "my heart aches and a drowsy numbness pains my sense as though of hemlock i had drunk or emptied some dull opiate to the drains one minute past and lethe wards had sunk",]
+
+with open("enable1.txt") as f:
+    d = set(map(lambda s: s.strip().lower(), f.readlines()))
+    d.update(['a', 'i'])
+
+def Affine (input):
+    input_number_arr = []
+    for char in input:
+        if char != " ":
+            input_number_arr.append(ord(char.lower()) - 96)
+        else:
+            input_number_arr.append(0)
+    solution = Decode(input_number_arr)
+    return solution
+
+def Decode(input):
+    str_out = ""
+    for a in range(1, 20):
+        for b in range(1,20):
+            for letter in input:
+                if letter != 0:
+                    str_out = str_out + chr((((letter * a) + b) % 26) + 96)
+                else:
+                    str_out = str_out + " "
+            if spellcheck(str_out):
+                return str_out
+            str_out = ""
+        
+def spellcheck(word_str):
+    words = word_str.split()
+    correct = 0
+    for word in words:
+        if any(word == a for a in d ):
+            correct += 1
+        else:
+            return
+    if correct == len(words):
+        return True
+
+
+print("Input text: {}".format(test_str[2]))
+start = time.time()
+print(Affine(test_str[2]))
+end = time.time()
+print("Decoding too {} seconds".format(end-start))
